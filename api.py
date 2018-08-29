7c84e842f8296164c8e10c81b1b2edb11b8b46c9
Fernando <fernando.mendez@atos.net>
2018-08-29 07:58:06 -0400

    Updated services

--- api.py
+++ api.py
@@ -1,18 +1,22 @@
-## This file is part of OFION process BPM.
-## 
-# Fernando Mendez - fernando.mendez@atos.net
-#
-############################################
 #-*- coding: UTF-8 -*-
+# OFION demo process BPM services.
+#
+# 
+#
+# Fernando Mendez - fernando.mendez@atos.net
+
 
 from flask import Flask,jsonify
 from flask_restful import Api, Resource, reqparse
 from flask import abort
-import readPDF
+import utils
 import json
 from flask import make_response
 from flask import request
 from flask import url_for
+import time
+import datetime
+
 
 app = Flask(__name__)
 
@@ -29,43 +33,61 @@ app = Flask(__name__)
 #     }
 # }
 
+# External functions from utils.py
+
+data_invoice = utils.processData()
+
 invoices = []
-
-data_invoice = readPDF.processData()
 decoded_data_invoice = json.loads(data_invoice)
-invoices.append(decoded_data_invoice)
 
-@app.route('/user/invoice/<int:id_user>/', methods=['GET'])
-def get_invoice(id_user):
+# This function get an invoice for an user id
+@app.route('/invoice/user/<int:id_user>', methods=['GET'])
+def get_user_invoice(id_user):
+    invoices.append(decoded_data_invoice)
     for i in invoices:
-        if str(i['Invoice']['id_user']) == str(id_user):
-            return jsonify({"invoice": i['Invoice']})
+        if str(i['invoice']['id_user']) == str(id_user):
+            return jsonify({"invoice": i['invoice']})
         else:
             abort(404)
 
-        
+# This funcition get all invoices from invoices list
+@app.route('/invoices', methods=['GET'])
+def get_invoices():
+    if len(invoices)==0:
+        abort(404)
+    else:
+         return jsonify({"invoice": invoices})
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error":"Not found invoice for the user selected"}),404)
 
-@app.route('/user/invoice', methods=['POST'])
+
+#This function generates a new invoice throw user parameters. 
+@app.route('/invoice/user', methods=['POST'])
 def create_invoice():
     if not request.json or not 'InvoiceID' in request.json:
         abort(404)
-    id = int(decoded_data_invoice["Invoice"]["id_user"])
+    date = datetime.datetime.now()
+    random_id = utils.generateRandomId()
 
+    #Payload for a new invoice
     invoice = {
-        "Url_invoice": "http://external_services_ofion_invoice.pdf",
-        "State": "SO France", 
-        "InvoiceID": request.json['InvoiceID'], 
-        "id_user": id + 1, 
-        "Name": "Mr.Duck Lucas", 
-        "Zone": "DisneyLand Paris", 
-        "Address": "23 East Road France", 
-        "Date": "26/02/2018"
+            "Url_invoice": request.json['Url_invoice'],
+            "State": request.json['State'], 
+            "InvoiceID": request.json['InvoiceID'], 
+            "id_user": random_id, 
+            "Name": request.json['Name'], 
+            "Zone": request.json['Zone'], 
+            "Address": request.json['Address'], 
+            "Date": date
+        
     }
+
     invoices.append(invoice)
+    print "Tam", len(invoices)
     return jsonify({"invoice": invoice}),201
+
 
 # This function generates a "public" version o an invoice to send to the client/services in order 
 # to avoid clients to be forced to construct URLs from the invoice id
