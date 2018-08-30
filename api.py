5a4b47314bb7b6054cb7652f3df244e1b44e6ae4
Fernando <fernando.mendez@atos.net>
2018-08-30 05:35:52 -0400

    # Fake function to invoke another service

--- api.py
+++ api.py
@@ -88,28 +88,27 @@ def create_invoice():
     print "Tam", len(invoices)
     return jsonify({"invoice": invoice}),201
 
-#Fake function to send data to another service
-@app.route('/invoice/send', methods=['POST','GET'])
+# Fake function to invoke another service
+
+@app.route('/post', methods=['GET'])
 def send_invoice():
 
-    URL = 'http://0.0.0.0:8081/invoice/send'
+    URL_EXTERNAL_SERVICE = 'http://0.0.0.0:8081/invoice/user'
 
     headers = {
         'Content-Type': 'application/json',
     }
 
-    data = {
-        "Address": "23 East Road France",  
-        "InvoiceID": "123456789", 
-        "Name": "Mr.Romeo Trump", 
-        "State": "EEUU", 
-        "Url_invoice": "http://external_services_ofion_invoice.pdf", 
-        "Zone": "New York"
-    }
+    for i in invoices:
+        if i <= len(invoices)-1 :
+            content = json.dumps(i['invoice'])
+            r = requests.post(URL_EXTERNAL_SERVICE, data=content, headers=headers)
 
-    content = json.dumps(data)
+    return jsonify({"invoice": invoices}),201
 
-    r = requests.post(URL, data=content, headers=headers)
+
+
+
     
 # This function generates a "public" version o an invoice to send to the client/services in order 
 # to avoid clients to be forced to construct URLs from the invoice id
