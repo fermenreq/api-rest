69ae178eea163de84e01c1c911dbb8f826e336c0
Fernando <fernando.mendez@atos.net>
2018-08-30 08:23:12 -0400

    Few changes project format

--- tests/post.sh
+++ tests/post.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 curl -i -H "Content-Type: application/json" -X POST -d '
-{
-	"Address": "23 East Road France",  
+{  "Address": "23 East Road France",  
     "InvoiceID": "123456789", 
     "Name": "Mr.Romeo Trump", 
     "State": "EEUU", 
     "Url_invoice": "http://external_services_ofion_invoice.pdf", 
     "Zone": "New York"
 }' http://0.0.0.0:8081/invoice/user
+
+
