14414a194b0236b3f0d81effb503146d0692887a
Fernando <fernando.mendez@atos.net>
2018-09-03 07:20:50 -0400

    Updated project folder

---
+++ install/basic-dep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install apache2 libapache2-mod-wsgi
+sudo /etc/init.d/apache2 restart
+
+sudo a2enmod wsgi
+
+
--- install/up.sh
+++ install/up.sh
@@ -4,5 +4,9 @@
 virtualenv .venv --always-copy
 
 pip install -r requirements.txt
+/etc/init.d/apache2 restart
+
+sudo a2enmod wsgi
+
 source .venv/bin/activate
 
